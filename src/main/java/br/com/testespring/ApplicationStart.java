266013da598ff414b6280e2ff1e2e8820aaabde3
JV01\dieison.grumovski <dieison.grumovski@fluig.com>
2018-03-26 15:46:06 -0300

    @Component, @Autowired e @SpringBootApplication

---
+++ src/main/java/br/com/testespring/ApplicationStart.java
@@ -0,0 +1,11 @@
+package br.com.testespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //@EnableAutoConfiguration @ComponentScan @Configuration
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
--- src/main/java/br/com/testespring/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/testespring/endpoint/StudentEndPoint.java
@@ -1,12 +1,16 @@
 package br.com.testespring.endpoint;
 
 import br.com.testespring.model.Student;
+import br.com.testespring.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static java.util.Arrays.asList;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -14,8 +18,12 @@ import java.util.List;
 @RequestMapping("student")
 public class StudentEndPoint {
 
+    @Autowired
+    private DateUtil dateUtil;
+
     @RequestMapping(method = RequestMethod.GET, path="/list")
     public List<Student> listAll(){
+        System.out.println("######################### - "+dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return asList(new Student("Maria"), new Student("João"));
     }
 }
